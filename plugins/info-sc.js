0dfbdb86a14a147a403645ca4c3ee1a41bba4ae8
zero7v <106769609+zero7v@users.noreply.github.com>
2022-06-06 00:14:51 +0530

    Update info-sc.js

--- plugins/info-sc.js
+++ plugins/info-sc.js
@@ -1,11 +1,11 @@
 import fs from 'fs'
 let handler = async (m, { conn }) => {
-conn.reply(m.chat, `*https://github.com/FahriAdison/Sad-Multi-Device*`, m, {
+conn.reply(m.chat, `*https://github.com/*`, m, {
 contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, description: null, 
 title: '',
 body: '',         
 previewType: 0, thumbnail: fs.readFileSync("./thumbnail.jpg"),
-sourceUrl: `https://github.com/FahriAdison`}}})
+sourceUrl: `https://github.com/n`}}})
 }
 handler.command = ['sc','script'] 
 handler.help = ['sc']
