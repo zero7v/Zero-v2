4c283be76a2f903147b79f294550ae3710568624
zero7v <106769609+zero7v@users.noreply.github.com>
2022-06-06 00:17:44 +0530

    Update config.js

--- config.js
+++ config.js
@@ -28,7 +28,7 @@ global.APIs = { // API Prefix
 }
 global.APIKeys = { // APIKey Here
   // 'https://website': 'apikey'
-  'https://api.xteam.xyz': 'YourApikey',
+  'https://api.xteam.xyz': 'd6fb31b64767b418',
   'https://zahirr-web.herokuapp.com': 'zahirgans',
   'https://api.zeks.me': 'apivinz',
   'https://pencarikode.xyz': 'pais',
